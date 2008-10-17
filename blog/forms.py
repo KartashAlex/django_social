cdd74f1c9f0e36be4ecf03312fd8df4c1aeddb03
Ivan Kuznetsov <ikenovodvorsky@gmail.com>
2008-10-17 11:39:07 +0400

    форма добавления обьявления

--- blog/forms.py
+++ blog/forms.py
@@ -6,10 +6,14 @@ from models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        exclude = ['author']
+        exclude = ['author', 'type']
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user')
+        try:
+            self.type = kwargs.pop('type')
+        except:
+            self.type = "Blog post"
         super(PostForm, self).__init__(*args, **kwargs)
         self.fields['group'].choices = [('', _('None'))] + [(g.id, g.name) for g in self.user.user_groups.all()]
         if not self.fields['group'].choices:
@@ -18,6 +22,8 @@ class PostForm(forms.ModelForm):
     def save(self):
         post = super(PostForm, self).save(commit=False)
         post.author = self.user
+
+        post.type = self.type
         post.save()
         
         return post
